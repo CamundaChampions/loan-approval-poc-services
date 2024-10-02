dd5ec22f09610c60fe9b14536d12044ef231b0b8
Sharma-abhi11 <saraswatabhi62@gmail.com>
2024-10-02 14:41:46 +0530

    Initial project setup and config

---
+++ loan-approval-poc-services/src/main/java/com/gen/loanApprovalPocServices/LoanApprovalPocServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gen.loanApprovalPocServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApprovalPocServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApprovalPocServicesApplication.class, args);
+    }
+
+}
